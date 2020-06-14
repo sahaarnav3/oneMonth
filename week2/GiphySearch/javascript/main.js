5ee8b6fa9a52860acdba8d0e0a24643d94d9f45f
sahaarnav3 <sahaarnav3@gmail.com>
2020-06-15 02:10:25 +0530

    :fire:

--- week2/GiphySearch/javascript/main.js
+++ week2/GiphySearch/javascript/main.js
@@ -1,5 +1,5 @@
-document.querySelector(".js-go").addEventListener('click', function(){
-    let input = document.querySelector("input").placeholder;
+document.querySelector(".js-go").addEventListener('click', function(e){
+    let input = document.querySelector("input").value;
     console.log(input);
     pushToDom(input);
 });
@@ -11,6 +11,24 @@ document.querySelector(".js-userinput").addEventListener('keyup', function(e) {
         pushToDom(input);
     }
 });
+
+//api link with key -- http://api.giphy.com/v1/gifs/search?q=cat&api_key=PscMUKBhvUny302aOXQ2x5xTWIJZrt7E
+let url = "http://api.giphy.com/v1/gifs/search?q=cat&api_key=PscMUKBhvUny302aOXQ2x5xTWIJZrt7E";
+
+// AJAX request
+let GiphyAJAXcall = new XMLHttpRequest();
+GiphyAJAXcall.open('GET', url);
+GiphyAJAXcall.send();
+
+GiphyAJAXcall.addEventListener('load', function(e) {
+    let data = e.target.response;
+    console.log(data);
+});
+
+
+
+
+
 
 
 function pushToDom(input){
