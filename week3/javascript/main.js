fdd8a2776828f4f69b09b2dc1d7af6d07ddf35ab
sahaarnav3 <sahaarnav3@gmail.com>
2020-06-17 22:47:18 +0530

    :star:

--- week3/javascript/main.js
+++ week3/javascript/main.js
@@ -3,7 +3,6 @@
 
 
 /* 2. Query Soundcloud API*/
-
 var SoundCloudAPI = {};
 
 SoundCloudAPI.init = function() {
@@ -19,20 +18,60 @@ SoundCloudAPI.getTrack = function(inputValue) {
       q: inputValue
     }).then(function(tracks) {
       console.log(tracks);
+      /* SoundCloudAPI.renderTracks(tracks)*/  // method 1
+      for(let i in tracks){
+        SoundCloudAPI.renderTracks(tracks[i]);  // method 2
+      }
     });
 }
 SoundCloudAPI.getTrack("Rilo Kiley");
 
-SoundCloudAPI.renderTracks = function() {
+SoundCloudAPI.renderTracks = function(track) {
 
+/*   track.forEach(function(track_) {      //method 1
+
+  }); */
+
+  console.log(track);
   let card = document.createElement('div');
   card.classList.add("card");
-  document.querySelector(".js-search-results").appendChild(card);
-  let image = document.createElement('div');
-  image.classList.add("image");
-  document.querySelector("").appendChild(image);
 
+  let imageDiv = document.createElement('div');
+  imageDiv.classList.add("image");
 
+  let image_img = document.createElement('img');
+  image_img.classList.add("image_img");
+  image_img.src = track.artwork_url || 'https://picsum.photos/200/200';
+
+  imageDiv.appendChild(image_img);
+
+  let content = document.createElement('div');
+  content.classList.add("content");
+
+  let header = document.createElement('div');
+  header.classList.add("header");
+  header.innerHTML = '<a href="'+track.permalink_url +'" target="_blank">' + track.title + '</a>';
+
+  let button = document.createElement('div');
+  button.classList.add('ui', 'bottom', 'attached', 'button', 'js-button');
+
+  let icon = document.createElement('i');
+  icon.classList.add('add', 'icon');
+  
+  let buttonText = document.createElement('span');
+  buttonText.innerHTML = "Add To Playlist";
+
+  content.appendChild(header);
+
+  button.appendChild(icon);
+  button.appendChild(buttonText);
+
+  card.appendChild(imageDiv);
+  card.appendChild(content);
+  card.appendChild(button);
+
+ document.querySelector('.js-search-results').appendChild(card); 
+  
 }
 
 SoundCloudAPI.renderTracks();
