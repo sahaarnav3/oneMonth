46c522cf9a26d401c0bc64461e56b86d87835b8d
sahaarnav3 <sahaarnav3@gmail.com>
2020-06-17 11:46:35 +0530

    :octocat:

--- week3/javascript/main.js
+++ week3/javascript/main.js
@@ -4,16 +4,25 @@
 
 /* 2. Query Soundcloud API*/
 
-SC.initialize({
-    client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
-});
+var SoundCloudAPI = {};
 
-// find all sounds of buskers licensed under 'creative commons share alike'
-SC.get('/tracks', {
-  q: 'rilo kiley'
-}).then(function(tracks) {
-  console.log(tracks);
-});
+SoundCloudAPI.init = function() {
+    SC.initialize({
+      client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
+  });
+} 
+SoundCloudAPI.init();
+
+SoundCloudAPI.getTrack = function(inputValue) {
+    // find all sounds of buskers licensed under 'creative commons share alike'
+    SC.get('/tracks', {
+      q: inputValue
+    }).then(function(tracks) {
+      console.log(tracks);
+    });
+}
+SoundCloudAPI.getTrack("Rilo Kiley");
+
 
 
 /* 3. Display the cards */
